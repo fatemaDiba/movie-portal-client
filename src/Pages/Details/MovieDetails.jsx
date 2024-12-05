5853e2eef4d1c3a6788392116c4d5eacd6616c40
Fatema Tabassum Diba <fatematabassum00700@gmail.com>
2024-12-06 01:09:01 +0600

    details,add and frontend

--- src/Pages/Details/MovieDetails.jsx
+++ src/Pages/Details/MovieDetails.jsx
@@ -1,20 +1,31 @@
+import { FaHeartCirclePlus } from "react-icons/fa6";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 const MovieDetails = () => {
   return (
-    <div className="container w-8/12 mx-auto">
-      <div className="hero bg-base-200 rounded-2xl">
-        <div className="hero-content flex-col lg:flex-row">
+    <div className="container w-8/12 mx-auto mb-20">
+      <div className="p-10 bg-base-200 rounded-2xl">
+        <div className="flex flex-col lg:flex-row gap-10 items-center">
           <img
             src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl w-[50%]"
+            className="w-[40%] rounded-lg shadow-2xl"
           />
-          <div>
-            <h1 className="text-3xl font-bold">The NoteBook</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+          <div className="space-y-3">
+            <h1 className="text-xl md:text-3xl font-bold mb-5">The NoteBook</h1>
+            <p className="text-base md:text-lg font-semibold">Title:</p>
+            <p className="text-base md:text-lg font-semibold">Genre:</p>
+            <p className="text-base md:text-lg font-semibold">Duration:</p>
+            <p className="text-base md:text-lg font-semibold">Release Year:</p>
+            <p className="text-base md:text-lg font-semibold">Rating:</p>
+            <p className="text-base md:text-lg font-semibold">Summary:</p>
+
+            <div className="flex flex-col md:flex-row gap-5 pt-10">
+              <button className="btn btn-primary">
+                Delete <RiDeleteBin6Fill className="text-lg" />
+              </button>
+              <button className="btn btn-primary">
+                Add To Favorite <FaHeartCirclePlus className="text-lg" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/privateRoutes/AddMovie.jsx
+++ src/privateRoutes/AddMovie.jsx
@@ -1,13 +1,41 @@
+import ReactStars from "react-rating-stars-component";
 const AddMovie = () => {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+
+  const handleAddBtn = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const poster = form.poster.value;
+    const title = form.title.value;
+    const genre = form.genre.value;
+    const duration = form.duration.value;
+    const year = form.year.value;
+
+    const summary = form.summary.value;
+
+    const movieAddData = {
+      poster,
+      title,
+      genre,
+      duration,
+      year,
+      summary,
+    };
+    console.log(movieAddData);
+  };
+
   return (
     <div>
-      <div className="container md:w-11/12 mx-auto">
+      <div className="container md:w-11/12 mx-auto mb-20">
         <div className="card bg-base-100 w-[70%] md:w-[50%] mx-auto shrink-0 shadow-2xl">
           <div className="card-body">
             <h2 className="font-bold text-center text-base md:text-xl">
               Add Your Movie Information Now
             </h2>
-            <form>
+            <form onSubmit={handleAddBtn}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Movie Poster</span>
@@ -29,10 +57,15 @@ const AddMovie = () => {
                   name="title"
                   placeholder="movie title"
                   className="input input-bordered mb-5"
+                  required
                 />
               </div>
-              <select className="select select-bordered w-full" required>
-                <option disabled selected>
+              <select
+                className="select select-bordered w-full"
+                required
+                name="genre"
+              >
+                <option value="" disabled selected>
                   Genre
                 </option>
                 <option>Horror</option>
@@ -52,8 +85,12 @@ const AddMovie = () => {
                   className="input input-bordered mb-5"
                 />
               </div>
-              <select className="select select-bordered w-full" required>
-                <option disabled selected>
+              <select
+                className="select select-bordered w-full"
+                required
+                name="year"
+              >
+                <option value="" disabled selected>
                   Release Year
                 </option>
                 <option>2020</option>
@@ -62,9 +99,24 @@ const AddMovie = () => {
                 <option>2023</option>
                 <option>2024</option>
               </select>
+
+              <ReactStars
+                count={5}
+                onChange={ratingChanged}
+                size={24}
+                activeColor="#ffd700"
+              />
+
+              <textarea
+                placeholder="Summery"
+                name="summary"
+                className="textarea textarea-bordered textarea-base w-full"
+                required
+              ></textarea>
+
               <div className="form-control mt-6 mb-4">
-                <button className="btn bg-orange-500 hover:bg-orange-600 text-white">
-                  Register
+                <button className="btn bg-purple-500 hover:bg-purple-600 text-white">
+                  Add Movie
                 </button>
               </div>
             </form>
