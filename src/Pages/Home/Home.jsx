9a604ddeb180bd2342cb9b851999001dc5b5cc71
Fatema Tabassum Diba <fatematabassum00700@gmail.com>
2024-12-05 23:00:42 +0600

    features

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import Features from "./UniqueSection/Features";
 import MoviesCard from "./moviesCard/MoviesCard";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Features></Features>
       <MoviesCard></MoviesCard>
     </div>
   );
---
+++ src/Pages/Home/UniqueSection/Features.jsx
@@ -0,0 +1,62 @@
+const Features = () => {
+  return (
+    <div className="w-10/12 mx-auto mb-20">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
+        <div className="justify-items-center">
+          <img
+            className=" w-[20%] md:w-[30%] mb-5"
+            src="/assets/earth.png"
+            alt=""
+          />
+          <h2 className="text-lg md:text-xl font-bold mb-3">Works Worldwide</h2>
+          <p className="text-sm md:text-base text-center">
+            No other free streaming service delivers more content to and from
+            more countries worldwide.
+          </p>
+        </div>
+        <div className="justify-items-center">
+          <img
+            className="w-[20%] md:w-[30%] mb-5"
+            src="/assets/video.png"
+            alt=""
+          />
+          <h2 className="text-lg md:text-xl font-bold mb-3">
+            Thousands of Titles
+          </h2>
+          <p className="text-sm md:text-base text-center">
+            Choose world-wide movies of your choice korean, chines , taiwan and
+            more all within one website.
+          </p>
+        </div>
+        <div className="justify-items-center">
+          <img
+            className="w-[20%] md:w-[30%] mb-5"
+            src="/assets/pound.png"
+            alt=""
+          />
+          <h2 className="text-lg md:text-xl font-bold mb-3">
+            Always 100% Free
+          </h2>
+          <p className="text-sm md:text-base text-center">
+            Welcome to instant gratification at its best. Watch now without any
+            payment or subscription and end the search for free movie websites.
+          </p>
+        </div>
+        <div className="justify-items-center">
+          <img
+            className="w-[20%] md:w-[30%] mb-5"
+            src="/assets/device.png"
+            alt=""
+          />
+          <h2 className="text-lg md:text-xl font-bold mb-3">Device-Friendly</h2>
+          <p className="text-sm md:text-base text-center">
+            Stream the good stuff from your favorite devices including Apple,
+            Android, Smart TVs and more.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
