0bd1f38dc1e3f780f05ca42d030d5668075d65b0
Fatema Tabassum Diba <fatematabassum00700@gmail.com>
2024-12-05 19:38:29 +0600

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,150 @@
+import { FaFacebook } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { IoLogoYoutube } from "react-icons/io5";
+import { FaTiktok } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="text-white bg-gray-900">
+      <div className="container mx-auto">
+        <div className="p-20">
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="text-xl md:text-2xl font-bold mb-2">MOVIE PORTAL</h3>
+            <p className="w-[45%] text-center text-sm md:text-base text-white/50">
+              A Free Theater for all has begun. Enjoy instant access to 6000+
+              Movies for the whole family anywhere, on any device.
+            </p>
+          </div>
+          <div className="border-t-2 border-[#09080F1A] mt-10">
+            <footer className="grid grid-cols-1 lg:grid-cols-4 gap-16 p-10 mx-auto">
+              <nav className="flex flex-col gap-1 ">
+                <p className="link link-hover text-base md:text-lg  hover:text-orange-400 text-white/90 mb-2">
+                  Company
+                </p>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  About
+                </a>
+                <a className="link link-hover  hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Career
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Our culture
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Giving
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  News
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Partners
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Movie Portal Blogs
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Advertise with Us
+                </a>
+              </nav>
+              <nav className="flex flex-col gap-1 ">
+                <p className="link link-hover text-base md:text-lg  hover:text-orange-400 text-white/90 mb-2">
+                  Movie portal Pass
+                </p>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Go premium
+                </a>
+                <a className="link link-hover  hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  MovieAmp
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Movie Labs
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Get Perks
+                </a>
+              </nav>
+              <nav className="flex flex-col gap-1 ">
+                <p className="link link-hover text-base md:text-lg  hover:text-orange-400 text-white/90 mb-2">
+                  Downloads
+                </p>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Movie Media Server
+                </a>
+                <a className="link link-hover  hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Movie Portal
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Movie Photos
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Movie Dash
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Where to Watch
+                </a>
+              </nav>
+              <nav className="flex flex-col gap-1 ">
+                <p className="link link-hover text-base md:text-lg  hover:text-orange-400 text-white/90 mb-2">
+                  Support
+                </p>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Finding Help
+                </a>
+                <a className="link link-hover  hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Support Library
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Community Forums
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Community Guidelines
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Billing Questions
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Status
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Bug Bounty
+                </a>
+                <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                  Get in Touch
+                </a>
+              </nav>
+            </footer>
+          </div>
+          <div className="border border-t-1 border-white/30 mb-10 mt-10 w-full"></div>
+          <div className=" flex justify-between items-center text-center text-sm md:text-base text-white/70">
+            <div className="flex gap-4">
+              <p className=" text-white/50">
+                © 2024 Adventure Page. All Rights Reserved.
+              </p>
+              <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                Privacy & Legal
+              </a>
+              <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                Accessibility
+              </a>
+              <a className="link link-hover hover:text-orange-400 text-white/50 text-sm md:text-base">
+                Manage Cookies
+              </a>
+            </div>
+
+            <div className="flex gap-3">
+              <FaFacebook className="text-2xl" />
+              <FaTwitter className="text-2xl" />
+              <IoLogoYoutube className="text-2xl" />
+              <RiInstagramFill className="text-2xl" />
+              <FaTiktok className="text-2xl" />
+              <FaLinkedinIn className="text-2xl" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
